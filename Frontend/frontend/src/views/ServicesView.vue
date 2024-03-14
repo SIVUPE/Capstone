d36432813f025b65d2227d81c68cdc8aec659f70
SIVUPE <sivuyisiwepeter79@gmail.com>
2024-03-14 11:27:03 +0200

    single

--- Frontend/frontend/src/views/ServicesView.vue
+++ Frontend/frontend/src/views/ServicesView.vue
@@ -1,25 +1,31 @@
 <template>
-    <div class="container">
-        <div class="row">
+    <div class="main">
+        <div class="container ">
+                <div class="row ">
             <div class="col">
-                <input type="text" placeholder="Search product by name" class="form-control">
+                <button class="btn ">Sorting by price</button>
             </div>
             <div class="col">
-                <button class="btn btn-success">Sorting by price</button>
+                <input type="text" placeholder="Search product by name" class="form-control" >
             </div>
-        </div>
-        <div class="row" v-if="services">
-            <Card v-for="service in services" :key="service.serviceID">
+            <div class="col">
+                <button class="btn ">Filter by items</button>
+            </div>
+            </div>
+        <div class="products">
+            <div class="row mx-5 gap-5 justify-content-center  " v-if="services">
+            <Card v-for="service in services" :key="service.servicesID" class="mb-3 ">
                 <template #cardHeader>
+                    <img :src="service.servicesUrl" class="car-img-top" alt="image" height="200">
                     <h4 class="card-title">{{ service.serviceName }}</h4>
+                    <h6 class="card-title">{{ service.serviceDescription }}</h6>
                 </template>
                 <template #cardBody>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
-                        Quantity: {{ service.serviceDescription }}
-                    </p>
-                    <p class="card-text text-dark bg-gradient bg-dark-subtle p-2">
+                    <p class="card-text text-dark  p-2">
                         Amount: R{{ service.price }}
                     </p>
+                    
+            
                     <router-link :to="{name: 'service', params: {id: service.serviceID}}">View More</router-link>
                 </template>
             </Card>
@@ -27,6 +33,8 @@
         <div class="row" v-else>
             <p class="lead">Loading</p>
         </div>
+        </div>
+    </div>
     </div>
 </template>
 <script>
@@ -36,15 +44,60 @@ export default {
     components: {
         Card
     },
-    computed:{
-        services(){
-            return this.$store.state.services
+    computed: {
+        services() {
+            return this.$store.state.services;
         }
     },
     mounted() {
-        this.$store.dispatch('fetchServices')
+        this.fetchServices();
+    },
+    methods: {
+        async fetchServices() {
+            try {
+                await this.$store.dispatch('fetchServices');
+            } catch (error) {
+                console.error('Error fetching services:', error);
+            }
+        }
     }
 }
 </script>
 <style scoped>
+.products{
+    margin-top:110px ;
+}
+.form-control{
+    width: 290px;
+    margin-left: 70px;
+}
+.btn {
+  border: 2px solid #f4f2ee;
+  color: #403e3a;
+  padding:  10px;
+  background-color: transparent;
+  margin-left: 50px;
+}
+.main{
+    height: 350vh;
+    background-color:#252926 ;
+}
+.mb-3 {
+  border: 2px solid #AF8C53;
+  border-radius: 10px;
+  background-color: #fff;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+}
+.card-title,
+.card-text {
+  text-transform: uppercase;
+  font-family: 'DM sans';
+  font-size: medium;
+  color: #AF8C53;
+}
+.card:hover {
+  transform: translateY(-5px);
+  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
 </style>
---
+++ Frontend/frontend/src/views/serviceView.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="product bg-image">
+        <div class="row justify-content-center">
+           <div class="col-7 text-black m-5">
+               <div class="row" >
+                {{ service }}
+                   <div class="col-7 d-flex align-items-center" v-if="service">
+                    <img :src="service.servicesUrl" class="car-img-top" alt="" height="380">
+                   </div>
+                   <div class="col-4 d-flex flex-column justify-content-between" id="divide" v-if="service">
+                       <div class="d-flex flex-column align-items-start">
+                           <h1 class="head">{{ service.serviceName }}</h1>
+                           <p class="lead opacity-75">"{{ service.serviceDescription }}"</p>
+                           <h3 class="product-price"></h3>
+                           <hr class=" w-100">
+                           <div class="mbc">
+                               <p> Amount: R{{ service.price }}</p>
+                           </div>
+                       </div>
+                       <div class="d-flex flex-column align-items-start">
+                           <button class="Portrait-btn p-2 ">Book</button>
+                       </div>
+                   </div>
+               </div>
+           </div>
+        </div>
+    </div>
+   </template>
+
+<script>
+// import store from 'vuex'
+    export default {
+        computed: {
+            service() {
+                return this.$store.state.service
+            }
+        },
+        mounted() {
+            this.$store.dispatch("fetchService", this.$route.params)
+            console.log('On mounted: ', this.service);
+            console.log('params id: ', this.$route.params);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
